966d284e676eb09cff850b472f7c6399e32787ba
KirillGul <guliev.kirill@yandex.ru>
2021-04-30 15:37:22 +0300

    Изм

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (empty($uri)) $uri = 'main';
 
 if ($uri == 'main') {
 
-    query($link);
+    query($link, $uri);
 
 } else {
 
@@ -16,13 +16,13 @@ if ($uri == 'main') {
 
     if (count($uriArr) == 2) {
         if (checkCat($uriArr['0']) AND checkProd($uriArr['1'])) {
-            query($link, $uriArr['0'], $uriArr['1']);
+            $page = query($link, $uriArr, 2);
         } else {
             $page = query($link, '404');
             header("HTTP/1.0 404 Not Found");
         }
     } elseif (count($uriArr) == 1 AND checkCat($uriArr['0'])) {
-        query($link, $uriArr['0']);
+        $page = query($link, $uriArr, 1);
     } else {
         $page = query($link, '404');
         header("HTTP/1.0 404 Not Found");
@@ -30,14 +30,32 @@ if ($uri == 'main') {
 
 }
 
-/*function query ($link, $uri) {
-    $query = "SELECT * FROM pagesj WHERE url='$uri'";
+function query ($link, $uri, $i=0) {
+
+    if ($i == 1) {
+        $query = "SELECT * FROM pagesj WHERE url='$uri[0]'";
+        $result = mysqli_query($link, $query) or die(mysqli_error($link));
+        return mysqli_fetch_assoc($result);
+    }
+
+    if ($i == 2) {
+
+        $query = "SELECT * FROM pagesj WHERE url='$uri[0]'";
+        $result = mysqli_query($link, $query) or die(mysqli_error($link));
+        $rezCat  = mysqli_fetch_assoc($result);
+
+        $query = "SELECT * FROM pagesj WHERE url='$uri[1]'";
+        $result = mysqli_query($link, $query) or die(mysqli_error($link));
+        $rezProd  = mysqli_fetch_assoc($result);
+    }
+
+    /*$query = "SELECT * FROM pagesj WHERE url='$uri'";
     $result = mysqli_query($link, $query) or die( mysqli_error($link) );
     //Преобразуем то, что отдала нам база в нормальный массив PHP $data:
-    return mysqli_fetch_assoc($result);
+    return mysqli_fetch_assoc($result);*/
 }
 
-$page = query($link, $uri);
+/*$page = query($link, $uri);
 
 if (!$page) {
     $page = query($link, '404');
